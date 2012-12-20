b87d38b649397be83000f53f9a94925a9f677eba
Shinta Smith <shinta.smith@rackspace.com>
2012-12-19 22:26:57 -0600

    script to insert Cloud Integration Billing team

---
+++ sqls/insertCIBTeam.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO tmv_team(teamname) VALUES ('Cloud Integration Billing');
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Bob Jacoby', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Chandra Addala', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Mike Sluyter', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Naresh Kurapati', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Prakash Tiwari', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Shinta Smith', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Srikanth Kambhampati', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Vinny Ly', 1);
+
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Courtney Taylor', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Matt Carter', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Shalini Komarla', 1);
