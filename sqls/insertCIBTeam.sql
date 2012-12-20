573d283b1b5f5fcc019c275f8c272bea2aa72640
Bill Smith <billsmithaustin@yahoo.com>
2012-12-19 22:31:06 -0600

    Don't assume the team ID is 1

--- sqls/insertCIBTeam.sql
+++ sqls/insertCIBTeam.sql
@@ -1,14 +1,15 @@
 
 INSERT INTO tmv_team(teamname) VALUES ('Cloud Integration Billing');
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Bob Jacoby', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Chandra Addala', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Mike Sluyter', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Naresh Kurapati', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Prakash Tiwari', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Shinta Smith', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Srikanth Kambhampati', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Vinny Ly', 1);
 
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Courtney Taylor', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Matt Carter', 1);
-INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Shalini Komarla', 1);
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Bob Jacoby',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Chandra Addala',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Mike Sluyter',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Naresh Kurapati',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Prakash Tiwari',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Shinta Smith',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Srikanth Kambhampati',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Vinny Ly',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Courtney Taylor',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Matt Carter',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
+INSERT INTO tmv_person(name, tmv_teamid) VALUES ('Shalini Komarla',  (select id from tmv_team where teamname = 'Cloud Integration Billing'));
