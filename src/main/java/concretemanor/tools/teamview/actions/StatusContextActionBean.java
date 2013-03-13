b00f00765c9670742580c889cb4b685849a65f71
Bill Smith <billsmithaustin@yahoo.com>
2013-03-13 18:58:40 -0500

    whitespace only

--- src/main/java/concretemanor/tools/teamview/actions/StatusContextActionBean.java
+++ src/main/java/concretemanor/tools/teamview/actions/StatusContextActionBean.java
@@ -9,36 +9,36 @@ import net.sourceforge.stripes.util.Log;
 import concretemanor.tools.teamview.domain.Team;
 
 public class StatusContextActionBean extends ActionBeanBase {
-	private static Log loggie = Log.getInstance(StatusContextActionBean.class);
+    private static Log loggie = Log.getInstance(StatusContextActionBean.class);
 
-	protected Integer teamId = null;
+    protected Integer teamId = null;
 
-	public StatusContextActionBean() {
-		super();
+    public StatusContextActionBean() {
+	super();
+    }
+
+    public Integer getTeamId() {
+	if (teamId == null) {
+	    teamId = getService().getAllTeams().get(0).getId();
 	}
+	return teamId;
+    }
 
-	public Integer getTeamId() {
-	    if (teamId == null) {
-			teamId = getService().getAllTeams().get(0).getId();
-		}
-		return teamId;
-	}
+    public void setTeamId(Integer teamId) {
+	loggie.debug("setTeam: teamId="+teamId);
+	this.teamId = teamId;
+    }
 
-	public void setTeamId(Integer teamId) {
-		loggie.debug("setTeam: teamId="+teamId);
-		this.teamId = teamId;
-	}
+    public List<Team> getAllTeams() {
+	List<Team> result = getService().getAllTeams();
+	return result;
+    }
 
-	public List<Team> getAllTeams() {
-		List<Team> result = getService().getAllTeams();
-		return result;
-	}
+    public Resolution refresh() {
+	return new ForwardResolution("/main.jsp");
+    }
 
-	public Resolution refresh() {
-		return new ForwardResolution("/main.jsp");
-	}
-
-	public Date getLastDate() {
-		return dateFrom(6);
-	}
+    public Date getLastDate() {
+	return dateFrom(6);
+    }
 }
